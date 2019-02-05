20835055722cb27607392b4b1caa66e8da4f7e91
Sebastian Haas <sehaas@deebas.com>
2019-02-06 00:43:34 +0100

    add decoder

--- yfmos.py
+++ yfmos.py
@@ -6,13 +6,43 @@
     Copyright 2019 Sebastian Haas <sehaas@deebas.com>
     https://github.com/sehaas/yfmos
     License: MIT
+
 """
 
 import sys
 import os
 import argparse
 from ConfigParser import SafeConfigParser
-from enum import Enum
+from enum import Enum, IntEnum
+
+class ManchesterDecode:
+    """
+        Decoder based on https://github.com/altelch/Somfy-RTS
+    """
+    def init(self, nextBit, secondPulse):
+        self.nextBit = nextBit
+        self.secondPulse = secondPulse
+        self.count = 0
+        self.bitvec = ""
+
+    def addShortPulse(self):
+        if self.secondPulse:
+            self.bitvec=self.bitvec+str(self.nextBit)
+            self.count=self.count+1
+            self.secondPulse=False
+        else:
+            self.secondPulse=True
+
+    def addLongPulse(self):
+        if not self.secondPulse:
+            return False
+        self.bitvec=self.bitvec+str(self.nextBit)
+        self.nextBit = self.nextBit ^ 1
+        self.count=self.count+1
+        return True
+
+    def get_bitvector(self):
+        return int(self.bitvec,base=2)
 
 class ManchesterEncode:
     def init(self, longPulse, shortPulse):
@@ -48,6 +78,17 @@ class Commands(Enum):
         except KeyError:
             raise ValueError()
 
+class States(IntEnum):
+    ST_UNKNOWN  = 0
+    ST_HW_SYNC1 = 1
+    ST_HW_SYNC2 = 2
+    ST_HW_SYNC3 = 3
+    ST_HW_SYNC4 = 4
+    ST_SW_SYNC1 = 5
+    ST_SW_SYNC2 = 6
+    ST_PAYLOAD = 7
+    ST_DONE = 8
+
 class Yfmos(object):
     CONFIG_FILE='.yfmosrc'
 
@@ -70,7 +111,122 @@ Available commands:
         getattr(self, args.command)()
 
     def init(self):
-        print('TBD - pars B1 string and init config/profile')
+        parser = argparse.ArgumentParser(
+        prog='%s init' % os.path.basename(sys.argv[0]),
+        description='Initalize the remote by with a B1 data string')
+        # TODO use --device to override saved device id
+        parser.add_argument('--device', '-d', help='Remote Handheld Addr.', type=self.__auto_int)
+        parser.add_argument('--profile', '-p', default='main')
+        parser.add_argument(metavar='...', dest='b1string', help='B1 string', nargs=argparse.REMAINDER)
+        args = parser.parse_args(sys.argv[2:])
+        if len(args.b1string) == 0:
+            parser.print_help()
+            exit(1)
+
+        options_debug = False
+        listOfElem = args.b1string
+        #print("ListofElem: %s" % listOfElem) #HK
+        iNbrOfBuckets = int(listOfElem[2])
+        #print("NumBuckets: %d" % iNbrOfBuckets) #HK
+        # Start packing
+        state = States.ST_UNKNOWN
+        pulse = {}
+        buckets = [None] * iNbrOfBuckets
+        for i in range(0, iNbrOfBuckets):
+            iValue = int(listOfElem[i + 3], 16)
+            buckets[i] = iValue
+            if iValue > 448 and iValue < 832:
+                pulse[i]="Short"
+            if iValue > 896 and iValue < 1664:
+                pulse[i]="Long"
+            if iValue > 1792 and iValue < 3328:
+                pulse[i]="HWsync"
+            if iValue > 3136 and iValue < 5824:
+                pulse[i]="SWsync"
+            if iValue > 25000:
+                pulse[i]="InterFrameGap"
+            if(options_debug):
+                print("Bucket %d: %s (%d)" % (i, pulse[i], iValue)) #HK
+        szDataStr = listOfElem[iNbrOfBuckets + 3]
+        iLength = len(szDataStr)
+        strNew = ""
+        decode=ManchesterDecode()
+        for i in range(0, iLength):
+            pos = i
+            strNew += szDataStr[pos:pos+1]
+            strNew += " "
+        if(options_debug):
+            print("Data: %s" % (strNew))
+        listOfElem = strNew.split()
+        iNbrOfNibbles = len(listOfElem)
+        for i in range(0,iNbrOfNibbles):
+            if(options_debug):
+                print pulse[int(listOfElem[i])]
+            if pulse[int(listOfElem[i])] is "HWsync":
+                #print "HWsync"
+                state=state+1
+                if state > States.ST_HW_SYNC4:
+                    state=States.ST_UNKNOWN
+            elif pulse[int(listOfElem[i])] is "SWsync" and state == States.ST_HW_SYNC4:
+                #print "SWsync"
+                state=States.ST_SW_SYNC2
+            elif pulse[int(listOfElem[i])] is "Long" and state == States.ST_SW_SYNC2:
+                #print "Sync Long",
+                state=States.ST_PAYLOAD
+                decode.init(1,True)
+            elif pulse[int(listOfElem[i])] is "Short" and state == States.ST_SW_SYNC2:
+                #print "Sync Short",
+                state=States.ST_PAYLOAD
+                decode.init(0,False)
+            elif state == States.ST_PAYLOAD:
+                if pulse[int(listOfElem[i])] is "Short":
+                    #print "Short",
+                    decode.addShortPulse()
+                elif pulse[int(listOfElem[i])] is "Long":
+                    #print "Long",
+                    if not decode.addLongPulse():
+                        state = States.ST_UNKNOWN
+                elif pulse[int(listOfElem[i])] is "InterFrameGap":
+                    #print "InterFrameGap"
+                    if decode.count==55:
+                        decode.bitvec=decode.bitvec+str(decode.nextBit)
+                    state = States.ST_DONE
+                else:
+                    state = States.ST_UNKNOWN
+            else:
+                state=States.ST_UNKNOWN
+            if(options_debug):
+                print("%d: %s" % (i,States(state)))
+
+        if(options_debug):
+            print(bin(decode.get_bitvector()))
+            print(hex(decode.get_bitvector()))
+        number = decode.get_bitvector()
+        frame = {}
+        frame[0] = (number>>48) & 0xff
+        frame[1] = (number>>40) & 0xff
+        frame[2] = (number>>32) & 0xff
+        frame[3] = (number>>24) & 0xff
+        frame[4] = (number>>16) & 0xff
+        frame[5] = (number>>8) & 0xff
+        frame[6] = number & 0xff
+        frame = self.__deobfuscate(frame)
+        self.__printFrame(frame)
+
+        config = SafeConfigParser()
+        config.read(self.CONFIG_FILE)
+        if not config.has_section(args.profile):
+            config.add_section(args.profile)
+        config.set(args.profile, "RollingCode", str(frame[2]<<8 | frame[3]))
+        if args.device > 0:
+                config.set(args.profile, "Device", "0x%06X" % (args.device))
+        else:
+                config.set(args.profile, "Device", "0x%02X%02X%02X" % (frame[4], frame[5], frame[6]))
+        config.set(args.profile, "Buckets", ",".join(map(str,buckets)))
+        for key in pulse:
+            config.set(args.profile, pulse[key], str(key))
+        with open(self.CONFIG_FILE, 'wb') as configfile:
+            config.write(configfile)
 
     def gen(self):
         parser = argparse.ArgumentParser(
@@ -81,6 +237,7 @@ Available commands:
         parser.add_argument('--profile', '-p', default='main')
         args = parser.parse_args(sys.argv[2:])
 
+        # TODO: check for existing CONFIG_FILE
         config = SafeConfigParser()
         config.read(self.CONFIG_FILE)
         buckets = map(int, config.get(args.profile, 'buckets').split(','))
@@ -136,6 +293,11 @@ Available commands:
 
     def __obfuscate(self, data):
         for i in range(1,len(data)):
+            data[i] = data[i] ^ data[i-1]
+        return data
+
+    def __deobfuscate(self, data):
+        for i in range(len(data)-1, 1, -1):
             data[i] = data[i] ^ data[i-1]
         return data
 
